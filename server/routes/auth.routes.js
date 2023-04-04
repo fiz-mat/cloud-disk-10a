752eb988b1c21e00dd1fc9ddb63fcad085a1f6ac
тут_ваше_Имя <__>
2023-04-04 08:28:25 +0600

    lsn9_createDir

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,6 +5,9 @@ const config = require('config')
 const jwt = require('jsonwebtoken')
 const {check, validationResult} = require('express-validator')
 const router = new Router()
+const authMiddleware = require('../middleware/auth.middleware')
+const fileService = require('../services/fileService')
+const File = require('../models/File')
 
 router.post('/registration',
     [
@@ -26,6 +29,7 @@ router.post('/registration',
             const hashPassword = await bcrypt.hash(password, 15)
             const user = new User({email, password: hashPassword})
             await user.save()
+            await fileService.createDir(new File({user: user.id, name: ''}))
             return res.json({message: "User was created"})
         } catch (e) {
             console.log(e)
@@ -45,6 +49,28 @@ router.post('/login',
             if (!isPassValid) {
                 return res.status(400).json({message: `Invalid password`})
             }
+            const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"})
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    diskSpace: user.diskSpace,
+                    usedSpace: user.usedSpace,
+                    avatar: user.avatar
+                }
+            })
+        } catch (e) {
+            console.log(e)
+            res.send({message: "Server error"})
+        }
+    })
+
+
+router.get('/auth', authMiddleware,
+    async (req, res) => {
+        try {
+            const user = await User.findOne({_id: req.user.id})
             const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"})
             return res.json({
                 token,
